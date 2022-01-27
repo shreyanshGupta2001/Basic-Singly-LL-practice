76dccfda5bd3a63d203f9c3ddc961c4b8587421a
shreyanshGupta2001 <91524915+shreyanshGupta2001@users.noreply.github.com>
2022-01-27 08:43:45 +0530

    Add files via upload

---
+++ Add Elements in LL.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+};
+
+Node* head;
+void insert(int x)
+{
+    Node* temp = new Node();
+    temp->data = x;
+    temp->next = NULL;
+
+    if(head==NULL)
+    {
+        head = temp;
+    }
+    else
+    {
+        Node* temp1 = head;
+        while ((temp1->next) != NULL)
+        {
+            temp1 = temp1->next;
+        }
+
+        temp1->next = temp;
+        
+    }    
+}
+
+void print()
+{
+    Node* temp = head;
+    while(temp != NULL)
+    {
+        cout<<(temp->data)<<" ";
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    head == NULL;
+    insert(2);
+    insert(10);
+    insert(5);
+    insert(8);
+    insert(9);
+    insert(20);
+
+    print();
+
+    return 0;
+}
